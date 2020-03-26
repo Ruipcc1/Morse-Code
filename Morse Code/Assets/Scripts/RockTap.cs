3c7b77afb1d62d0f91951a7a5799b1f039b09539
Rui <ruipcc1@gmail.com>
2020-03-26 23:58:07 +0000

    Morse Code Game added

---
+++ Morse Code/Assets/Scripts/RockTap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockTap : MonoBehaviour
+{
+    public float tapTime;
+    public bool tap;
+    public bool dash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)) {
+            tapTime += Time.deltaTime;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            tapTime = 0f;
+            MorseDotDash();
+        }
+    }
+
+    void MorseDotDash()
+    {
+        if(tapTime < 0.5f)
+        {
+
+        }
+    }
+}
